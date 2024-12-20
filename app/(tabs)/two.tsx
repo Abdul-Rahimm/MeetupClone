e2f1c0860169978540aa32991c819d9ad0d486d2
Abdul-Rahimm <k213951@nu.edu.pk>
2024-12-21 03:13:34 +0500

    sorting etc

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,22 +1,29 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { Text, View } from 'react-native';
 
 export default function Home() {
+  const stringsArray = ['zpple', 'banana', 'cherry', 'date', 'elderberry'];
+  const nums = [1, 2, 3, 4, 67, 5, 4, 3, 2, 1];
+  const obj = [
+    { name: 'rahim', age: 100 },
+    { name: 'ahim2', age: 20 },
+    { name: 'bazzz', age: 20 },
+  ];
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab Two' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
+      <Stack.Screen options={{ title: 'Learning' }} />
+      <View>
+        {obj
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .sort((b, a) => a.age - b.age)
+          .map((item) => (
+            <View>
+              <Text>{item.name}</Text>
+              <Text>{item.age}</Text>
+            </View>
+          ))}
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,5 +1,5 @@
 import { useLocalSearchParams } from 'expo-router';
-import { Text } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import events from '../assets/events.json';
 import EventCard from '~/components/EventCard';
 
@@ -8,5 +8,16 @@ export default function EventPage() {
   const eventNumber = parseInt(id);
   const event = events[eventNumber - 1];
 
-  return <EventCard data={event} />;
+  return (
+    <>
+      <EventCard data={event} />;{/* Footer */}
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
+        <Text className="text-xl font-semibold">Free</Text>
+
+        <Pressable className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
+      </View>
+    </>
+  );
 }
